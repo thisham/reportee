16f5a422250c27d5fc96575d066ee6172bebebda
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-03-10 01:29:19 +0700

    feat: add rn-navigation as main navigator

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { Navigation } from 'react-native-navigation';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('com.cpeasant.reportee', App());
